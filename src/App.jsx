b274ece9605fc661a2bf3b0f5167a1597a6927a3
Egor Toropov <edoch@yandex.ru>
2024-01-10 20:46:32 +0300

    start project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from './views/Header/Header'
+const App = () => (
+  <>
+    <Header />
+    <main>
+      <p>Текст</p>
+    </main>
+    <footer>
+      Текст2
+    </footer>
+
+  </>
+);
+
+export default App;
